97f0af9ca3ecaae922b7cbe9831de50b74466b2d
shanestarkweather <shane@shanestarkweather.com>
2021-02-10 10:58:06 -0800

    added confirmation components

---
+++ src/SeniorConfirmation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function SeniorConfirmation() {
+	return (
+		<div>
+			<img></img>
+			<h2>Thank you for the submission!</h2>
+			<p>
+				Our volunteer will reach you within 1-3 business days by your preferred
+				method of contact.
+			</p>
+		</div>
+	);
+}
+
+export default SeniorConfirmation;
