b6b804aa545f791a2cc00afbb0a906fe0a456579
Joshua Favre <joshuaf@MacBook-Pro.local>
2021-02-11 11:11:05 -0600

    styled footer, add a tag links, add id to navie to scroll to

--- src/Footer.js
+++ src/Footer.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import arrow from './images/upArrow.svg';
 import './Footer.css';
-import logo from './images/COVAIDLogo.png';
 
 function Footer() {
-    return (
-			<div className='foot'>
-				<p className='logo'>COV-AID</p>
+	return (
+		<div className='foot'>
+			<p className='logo'>COV-AID</p>
+			<a href='#topper'>
 				<img src={arrow} className='arrow' alt='up arrow' />
 				<br />
 				<br />
 				<p className='top'>Top</p>
-			</div>
-		);
+			</a>
+		</div>
+	);
 }
 
 export default Footer;
