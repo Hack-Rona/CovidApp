3d253b8b788a54a71243d83631a32552133edef8
shanestarkweather <shane@shanestarkweather.com>
2021-02-10 22:27:55 -0800

    more css logo in footer confirmations done

--- src/Footer.js
+++ src/Footer.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import arrow from './images/upArrow.svg';
 import './Footer.css';
+import logo from './images/COVAIDLogo.png';
 
 function Footer() {
-    return (
-        <footer className='foot'>
-            <p>COV-AID</p>
-            <img src={arrow} className='arrow' alt='up arrow'/>
-            <p className='arrow'>Top</p>
-        </footer>
-    );
+	return (
+		<footer className='foot'>
+			<img className='logo' src={logo}></img>
+			<img src={arrow} className='arrow' alt='up arrow' />
+			<p className='arrow'>Top</p>
+		</footer>
+	);
 }
 
 export default Footer;
--- src/VolunteerConfirmation.js
+++ src/VolunteerConfirmation.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import volconfirm from './images/volunteerconfirm.svg';
+import Navie from './Navie';
+import Footer from './Footer';
 
 function VolunteerConfirmation() {
 	return (
 		<div>
-			<img></img>
-			<h2>
-				We appreciate the desire to volunteer with people who really need you!
-			</h2>
-			<p>Give us 1 to 3 business days to email you back.</p>
-			<p>Thank you!!!</p>
+			<Navie path='/navbar' component={Navie} />
+			<div>
+				<img src={volconfirm}></img>
+				<h2>
+					We appreciate the desire to volunteer with people who really need you!
+				</h2>
+				<p>Give us 1 to 3 business days to email you back.</p>
+				<p>Thank you!!!</p>
+			</div>
+			<Footer path='/footer' component={Footer} />
 		</div>
 	);
 }
