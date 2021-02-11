c2f7f5b09b91d17e0d9b1d93748d306690eeee74
Nicholas Pretel <nicholaspretel@Nicholass-MBP.lan>
2021-02-11 00:34:02 -0500

    Add links and text to carousel

--- src/Home.js
+++ src/Home.js
@@ -13,24 +13,24 @@ function Home(props) {
 
   // }
 
-  useEffect(() => {
-    const url =
-      "https://api.covid19tracking.narrativa.com/api/country/usa?date_from=2020-03-20&date_to=2020-12-31";
+  // useEffect(() => {
+  //   const url =
+  //     "https://api.covid19tracking.narrativa.com/api/country/usa?date_from=2020-03-20&date_to=2020-12-31";
 
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setNews(data);
-      })
-      .catch(() => {
-        setError(true);
-      });
-  }, []);
+  //   fetch(url)
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       console.log(data);
+  //       setNews(data);
+  //     })
+  //     .catch(() => {
+  //       setError(true);
+  //     });
+  // }, []);
 
-  if (error) {
-    return <h3>Sorry, there was a problem. Please refresh the page.</h3>;
-  }
+  // if (error) {
+  //   return <h3>Sorry, there was a problem. Please refresh the page.</h3>;
+  // }
 
   return (
     <div>
